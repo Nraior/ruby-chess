a19af7e22116b399ee986415bf1519cfc0bcaf90
DN <dnowakowski200@gmail.com>
2025-05-03 15:25:57 +0200

    standard & kill moves for pawn

--- lib/ruby_chess/pawn.rb
+++ lib/ruby_chess/pawn.rb
@@ -1,21 +1,48 @@
 require_relative 'figure'
 class Pawn < Figure
   def available_moves(board)
+    arr = []
+    standard_moves = forward_moves(board)
+    arr.push(standard_moves)
+    kill_moves = available_kill_moves(board)
+
+    arr.push(kill_moves) # unless available_kill_moves(board).empty?
+
+    arr.flatten(1)
+  end
+
+  def forward_moves(board)
     moves = []
     fields = board.fields
 
-    (1..2).each do |n|
+    ending_iterator_adder = moves_count.zero? ? 1 : 0
+
+    start = @y + @direction
+    end_index = start + (@direction * ending_iterator_adder)
+
+    loop_iterator = start > end_index ? start.downto(end_index) : (start..end_index)
+
+    loop_iterator.each do |n|
       x_pos = @x
-      # p n
-      y_pos = @y + (@direction * n)
-      next unless board.valid_move?(y_pos, x_pos)
 
-      p y_pos
-      p fields[y + (@direction * n)][@x].occupying.nil?
+      next unless board.valid_move?(n, x_pos)
 
-      moves.push([y_pos, x_pos]) if fields[y + (@direction * n)][@x].occupying.nil?
-      # p board[y + (@direction * 1)][@x].occupying
+      return moves unless fields[n][x_pos].occupying.nil?
+
+      moves.push([x_pos, n])
     end
+    moves
+  end
+
+  def available_kill_moves(board)
+    moves = []
+    fields = board.fields
+
+    upper_left_valid = board.valid_move?(@x - 1, @y + @direction)
+    upper_right_valid = board.valid_move?(@x + 1, @y + @direction)
+    moves.push([@x - 1, @y + @direction]) if upper_left_valid && !fields[@y + @direction][@x - 1].occupying.nil?
+    moves.push([@x + 1, @y + @direction]) if upper_right_valid && !fields[@y + @direction][@x + 1].occupying.nil?
+
     moves
   end
 end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -3,21 +3,21 @@ describe Pawn do
   subject(:pawn) { described_class.new(1, 3, -1) }
   let(:another_figure) { double('figure', { occupying: true }) }
   let(:empty_field) { double('empty_field', { occupying: nil }) }
-  let(:small_board) { double('small_board') }
+  let(:board) { double('board') }
   describe '#available_moves' do
     before do
-      allow(small_board).to receive(:fields).and_return([[nil, another_figure, nil],
-                                                         [nil, another_figure, nil],
-                                                         [nil, another_figure, nil],
-                                                         [nil, pawn, nil]])
-      allow(small_board).to receive(:width).and_return(3)
-      allow(small_board).to receive(:height).and_return(4)
-      allow(small_board).to receive(:valid_move?).and_return(true)
+      allow(board).to receive(:fields).and_return([[nil, another_figure, nil],
+                                                   [nil, another_figure, nil],
+                                                   [empty_field, another_figure, empty_field],
+                                                   [nil, pawn, nil]])
+      allow(board).to receive(:width).and_return(3)
+      allow(board).to receive(:height).and_return(4)
+      allow(board).to receive(:valid_move?).and_return(true)
     end
 
     context('when its blocked and pawn is at the bottom') do
       it('returns nothing') do
-        available_moves = pawn.available_moves(small_board)
+        available_moves = pawn.available_moves(board)
         expect(available_moves).to eq([])
       end
     end
@@ -26,24 +26,61 @@ describe Pawn do
       subject(:pawn) { described_class.new(1, 0, 1) }
 
       before do
-        allow(small_board).to receive(:fields).and_return([[nil, pawn, nil],
-                                                           [nil, another_figure, nil],
-                                                           [nil, another_figure, nil],
-                                                           [nil, another_figure, nil]])
+        allow(board).to receive(:fields).and_return([[nil, pawn, nil],
+                                                     [empty_field, another_figure, empty_field],
+                                                     [empty_field, another_figure, empty_field],
+                                                     [nil, another_figure, nil]])
       end
       it('returns nothing') do
-        available_moves = pawn.available_moves(small_board)
+        available_moves = pawn.available_moves(board)
         expect(available_moves).to eq([])
       end
     end
 
-    context('when its first move') do
+    context('when its first move and forward fields are free') do
+      before do
+        allow(board).to receive(:fields).and_return([[nil, nil, nil],
+                                                     [nil, empty_field, nil],
+                                                     [empty_field, empty_field, empty_field],
+                                                     [nil, pawn, nil]])
+      end
       it('returns forward & two_forward moves') do
+        result = pawn.available_moves(board)
+        expect(result).to eq([[1, 2], [1, 1]])
+      end
+    end
+
+    context 'when its second move and forward field is available ' do
+      before do
+        allow(board).to receive(:fields).and_return([[nil, nil, nil],
+                                                     [empty_field, empty_field, empty_field],
+                                                     [empty_field, empty_field, empty_field],
+                                                     [nil, pawn, nil]])
+        allow(pawn).to receive(:moves_count).and_return(1)
+      end
+
+      it 'returns forward field' do
+        result = pawn.available_moves(board)
+        expect(result).to eq([[1, 2]])
+      end
+    end
+
+    context 'when it can kill another figures' do
+      before do
+        allow(board).to receive(:fields).and_return([[nil, nil, nil],
+                                                     [nil, empty_field, nil],
+                                                     [another_figure, another_figure, another_figure],
+                                                     [nil, pawn, nil]])
+      end
+      it 'returns diagonal moves as kill' do
+        result = pawn.available_moves(board)
+        expect(result).to eq([[0, 2], [2, 2]])
       end
     end
 
     context 'when it has en passant available' do
-      it('returns en passant move')
+      xit('returns en passant move') do
+      end
     end
   end
 end
