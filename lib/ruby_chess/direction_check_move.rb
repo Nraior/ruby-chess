cbe98bda476f6f0a8c125afaa1ba66c5ab2e7608
DN <dnowakowski200@gmail.com>
2025-05-07 23:02:51 +0200

    initial module for CROSS moves etc

---
+++ lib/ruby_chess/direction_check_move.rb
@@ -0,0 +1,39 @@
+module DirectionCheckMove
+  def cross_move(board)
+    moves = []
+
+    left_moves = check_direction(board, x, y)
+    right_moves = check_direction(board, x, y, [], 1, 0)
+    up_moves = check_direction(board, x, y, [], 0, -1)
+    down_moves = check_direction(board, x, y, [], 0, 1)
+
+    moves.push(left_moves).push(right_moves).push(up_moves).push(down_moves)
+
+    moves.flatten(1)
+  end
+
+  def diagonal_moves(board)
+    moves = cross_move
+
+    left_up_moves = check_direction(board, x, y, -1, -1)
+    right_up_moves = check_direction(board, x, y, [], 1, -1)
+    left_down_moves = check_direction(board, x, y, [], -1, 1)
+    left_right_moves = check_direction(board, x, y, [], 1, 1)
+
+    moves.push(left_up_moves).push(right_up_moves).push(left_down_moves).push(left_right_moves)
+
+    moves.flatten(1)
+  end
+
+  private
+
+  def check_direction(board, x, y, moves = [], x_direction = -1, y_direction = 0)
+    return moves if !board.valid_move?(x + x_direction,
+                                       y + y_direction) && !board.enemy_at_position?(self, x + x_direction,
+                                                                                     y + y_direction)
+
+    moves.push([x + x_direction, y + y_direction])
+
+    check_direction(board, x + x_direction, y + y_direction, moves, x_direction, y_direction)
+  end
+end
