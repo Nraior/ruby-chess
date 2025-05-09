405913f9e09b85c5f15b28d56031b1c63f22840c
DN <dnowakowski200@gmail.com>
2025-05-09 20:00:22 +0200

    horse figure

--- lib/ruby_chess/figures/horse.rb
+++ lib/ruby_chess/figures/horse.rb
@@ -1,10 +1,22 @@
 require_relative 'figure'
 class Horse < Figure
-  def available_moves
+  def available_moves(board)
+    vectors_to_check = [[-1, -2], [-2, -1], [1, -2], [2, -1], [-1, 2], [-2, 1], [2, 1], [1, 2]]
+    moves = []
+    vectors_to_check.each do |vector|
+      checked = field(vector[0], vector[1], board)
+      moves.push(checked)
+    end
+    moves.compact
   end
 
   private
 
-  def check_board(x, y)
+  def field(x_vector, y_vector, board)
+    return nil unless board.valid_move?(x + x_vector, y + y_vector)
+
+    fig = board.figure_at_position(x + x_vector, y + y_vector)
+
+    [x + x_vector, y + y_vector] if fig.nil? || enemy?(fig)
   end
 end
--- spec/horse_spec.rb
+++ spec/horse_spec.rb
@@ -1,2 +1,68 @@
+require_relative '../lib/ruby_chess/figures/horse'
 describe Horse do
+  subject(:horse) { described_class.new(2, 2, 1) }
+  let(:board) { double('board') }
+  let(:empty_field) { double('empty_field', { occupying: nil }) }
+  let(:occupied_figure) { double('pawn', { direction: -1 }) }
+  let(:another_figure) { double('figure', { occupying: occupied_figure }) }
+
+  before do
+    allow(board).to receive(:fields).and_return([[empty_field, empty_field, empty_field, empty_field, empty_field],
+                                                 [empty_field, empty_field, empty_field, empty_field, empty_field],
+                                                 [empty_field, empty_field, horse, empty_field, empty_field],
+                                                 [empty_field, empty_field, empty_field, empty_field, empty_field],
+                                                 [empty_field, empty_field, empty_field, empty_field, empty_field]])
+    allow(board).to receive(:valid_move?) do |x, y|
+      x >= 0 && y >= 0 && y < board.fields.length && x < board.fields[0].length
+    end
+
+    allow(board).to receive(:figure_at_position) do |x, y|
+      board.fields[y][x].occupying
+    end
+  end
+
+  context 'when its empty' do
+    it 'returns L shape positions' do
+      positions = horse.available_moves(board)
+      expect(positions).to eq([[1, 0], [0, 1], [3, 0], [4, 1], [1, 4], [0, 3], [4, 3], [3, 4]])
+    end
+  end
+
+  context 'when enemies are on position' do
+    before do
+      allow(board).to receive(:fields).and_return([[empty_field, another_figure, empty_field, another_figure, empty_field],
+                                                   [another_figure, empty_field, empty_field, empty_field,
+                                                    another_figure],
+                                                   [empty_field, empty_field, horse, empty_field, empty_field],
+                                                   [another_figure, empty_field, empty_field, empty_field,
+                                                    another_figure],
+                                                   [empty_field, another_figure, empty_field, another_figure,
+                                                    empty_field]])
+    end
+
+    it 'returns L shape positions' do
+      positions = horse.available_moves(board)
+      expect(positions).to eq([[1, 0], [0, 1], [3, 0], [4, 1], [1, 4], [0, 3], [4, 3], [3, 4]])
+    end
+  end
+
+  context 'when allies are on positions' do
+    let(:occupied_figure) { double('pawn', { direction: 1 }) }
+    let(:own_figure) { double('figure', { occupying: occupied_figure }) }
+
+    before do
+      allow(board).to receive(:fields).and_return([[empty_field, own_figure, empty_field, own_figure, empty_field],
+                                                   [own_figure, empty_field, empty_field, empty_field,
+                                                    own_figure],
+                                                   [empty_field, empty_field, horse, empty_field, empty_field],
+                                                   [own_figure, empty_field, empty_field, empty_field,
+                                                    own_figure],
+                                                   [empty_field, own_figure, empty_field, own_figure,
+                                                    empty_field]])
+    end
+    it 'returns nothing' do
+      positions = horse.available_moves(board)
+      expect(positions).to eq([])
+    end
+  end
 end
