9f098522c5febde909320aa8db2c3214f9df0b8a
DN <dnowakowski200@gmail.com>
2025-05-19 23:08:49 +0200

    two more test cases for pawn legal moves

--- spec/integration/pawn_own_checkmate_prevent_spec.rb
+++ spec/integration/pawn_own_checkmate_prevent_spec.rb
@@ -5,34 +5,63 @@ require './lib/ruby_chess/board'
 require './lib/ruby_chess/field'
 
 describe Pawn do
-  context 'when one standard move would result in checkmate' do
-    subject(:pawn) { described_class.new(1, 1, -1) }
-    let(:pawn_field) { Field.new(nil, nil) }
-    let(:empty_field) { Field.new(nil, nil) }
-    let(:king_field) { Field.new(nil, nil) }
-    let(:enemy_rook_field_first) { Field.new(nil, nil) }
-    let(:enemy_rook_field_second) { Field.new(nil, nil) }
+  let(:king) { King.new(0, 0, - 1) }
+  let(:enemy_rook_first) { Rook.new(1, 0, 1) }
+  let(:enemy_rook_second) { Rook.new(2, 0, 1) }
+  let(:board) { Board.new(2, 2) }
+  subject(:pawn) { described_class.new(1, 1, -1) }
+  let(:pawn_field) { Field.new(nil, nil) }
+  let(:king_field) { Field.new(nil, nil) }
+  let(:enemy_rook_field_first) { Field.new(nil, nil) }
+  let(:enemy_rook_field_second) { Field.new(nil, nil) }
 
-    let(:king) { King.new(0, 0, - 1) }
-    let(:enemy_rook_first) { Rook.new(1, 0, 1) }
-    let(:enemy_rook_second) { Rook.new(2, 0, 1) }
-    let(:board) { Board.new(8, 7) }
-
+  before do
+    allow(board).to receive(:fields).and_return([[king_field, enemy_rook_field_first, enemy_rook_field_second, Field.new(nil, nil)],
+                                                 [Field.new(nil, nil), pawn_field,
+                                                  Field.new(nil, nil), Field.new(nil, nil)]])
+    king_field.occupy(king)
+    enemy_rook_field_first.occupy(enemy_rook_first)
+    pawn_field.occupy(pawn)
+    allow(pawn).to receive(:moves_count).and_return(1)
+  end
+  context 'when cant prevent checkmate' do
     before do
-      king_field.occupy(king)
-      enemy_rook_field_first.occupy(enemy_rook_first)
       enemy_rook_field_second.occupy(enemy_rook_second)
-      pawn_field.occupy(pawn)
-      allow(board).to receive(:fields).and_return([[king_field, enemy_rook_field_first, enemy_rook_field_second, empty_field],
-                                                   [empty_field, pawn_field,
-                                                    empty_field, empty_field]])
-      allow(pawn).to receive(:moves_count).and_return(1)
-      allow(OwnChekmateChecker).to receive(:will_cause_own_checkmate?).and_call_original
-      allow(OwnChekmateChecker).to receive(:en_passant_cause_own_checkmate?).and_call_original
     end
-    it "doesn't return that move " do
+    it "doesn't return any move " do
       moves = pawn.legal_moves(board)
       expect(moves).to eq([])
+    end
+  end
+
+  context 'when already blocking checkmate' do
+    let(:enemy_rook_first) { Rook.new(3, 1, 1) }
+    let(:king) { King.new(0, 1, - 1) }
+
+    before do
+      allow(board).to receive(:fields).and_return([[Field.new(nil, nil), Field.new(nil, nil), Field.new(nil, nil), Field.new(nil, nil)],
+                                                   [king_field, pawn_field, Field.new(nil, nil),
+                                                    enemy_rook_field_first]])
+    end
+
+    it 'doesnt allow move forward' do
+      moves = pawn.legal_moves(board)
+      expect(moves).to eq([])
+    end
+  end
+
+  context 'when can prevent checkmate by kill' do
+    let(:enemy_rook_first) { Rook.new(0, 0, 1) }
+    let(:king) { King.new(0, 1, - 1) }
+
+    before do
+      allow(board).to receive(:fields).and_return([[Field.new(nil, nil), Field.new(nil, nil)],
+                                                   [king_field, pawn_field]])
+    end
+
+    it 'returns kill move' do
+      moves = pawn.legal_moves(board)
+      expect(moves).to eq([[1, 0]])
     end
   end
 end
