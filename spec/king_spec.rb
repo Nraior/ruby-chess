75aaea5d21555806e5b960cf98361bd50ec255dd
DN <dnowakowski200@gmail.com>
2025-05-20 22:04:18 +0200

    king test finished

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -26,6 +26,7 @@ describe King do
     allow(cross_figure).to receive(:moves_count).and_return(0)
     allow(board).to receive(:any_team_figures_aims_at_pos?).and_return(false)
     allow(board).to receive(:width).and_return(8)
+    allow(cross_figure).to receive(:is_a?).with(Rook).and_return(true)
   end
 
   context 'when its alone' do
@@ -40,7 +41,6 @@ describe King do
                                                     empty_field],
                                                    [empty_field, empty_field, empty_field, empty_field,
                                                     empty_field]])
-      allow(cross_figure)
       allow(board).to receive(:width).and_return(5)
     end
     it 'returns neighboring positions' do
@@ -51,7 +51,6 @@ describe King do
 
   context 'when castling is available' do
     before do
-      allow(cross_figure).to receive(:is_a?).with(Rook).and_return(true)
     end
     it 'returns castling moves' do
       moves = king.available_moves(board)
@@ -60,26 +59,58 @@ describe King do
   end
 
   context 'when king already moved' do
+    before do
+      king.proceed_move(4, 0)
+    end
     it "doesn't return castle moves" do
+      moves = king.available_moves(board)
+      expect(moves).to eq([[3, 0], [5, 0]])
     end
   end
 
-  context 'when rooks moved once' do
+  context 'when rook moved once' do
+    before do
+      allow(cross_figure).to receive(:moves_count).and_return(1)
+    end
+    it "doesn't return its castle move" do
+      moves = king.available_moves(board)
+      expect(moves).to eq([[3, 0], [5, 0]])
+    end
+  end
+
+  context 'when one rook moved once' do
+    let(:cross_figure_2) { double('rook', { direction: 1 }) }
+    let(:own_field_2) { double('figure', { occupying: cross_figure_2 }) }
+
+    before do
+      allow(board).to receive(:fields).and_return([[own_field, empty_field, empty_field, empty_field, king_field, empty_field,
+                                                    empty_field, own_field_2]])
+      allow(cross_figure_2).to receive(:moves_count).and_return(1)
+    end
+
+    it 'returns only castle move' do
+      moves = king.available_moves(board)
+      expect(moves).to eq([[3, 0], [5, 0], [2, 0]])
+    end
+  end
+
+  context 'when road to castle is checked' do
+    subject(:king) { described_class.new(4, 1, 1) }
+    let(:enemy_blocking_field) { double('enemy_blocking_field', { direction: -1 }) }
+    let(:enemy_cross_field) { double('figure', { occupying: enemy_blocking_field }) }
+    before do
+      allow(board).to receive(:fields).and_return([[empty_field, empty_field, empty_field, empty_field, empty_field, empty_field,
+                                                    empty_field, enemy_cross_field], [own_field, empty_field, empty_field, empty_field, king_field, empty_field,
+                                                                                      empty_field, own_field]])
+      allow(enemy_blocking_field).to receive(:available_moves).and_return([
+                                                                            [1, 0], [7, 0]
+                                                                          ])
+      allow(board).to receive(:any_team_figures_aims_at_pos?).and_return(true)
+    end
+
     it "doesn't return castle moves" do
-    end
-  end
-
-  context 'when its checked' do
-    context 'when its empty between rook' do
-      it "doesn't return castle moves" do
-      end
-    end
-    it "returns available moves that doesn't end in check-mate " do
-    end
-  end
-
-  context 'when its check-mate' do
-    it 'returns no move' do
+      moves = king.available_moves(board)
+      expect(moves).to eq([[3, 0], [4, 0], [5, 0], [3, 1], [5, 1]])
     end
   end
 end
